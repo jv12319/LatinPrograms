d950d3b8783c90fcaf77749f441d7f6192e9952b
Joey <joey83891@gmail.com>
2026-07-09 21:54:04 -0400

    Created semi-functional menu in main file, added declension dictionaries in my latindata file. More progress O amIcI vidEbImus

--- LatinData.py
+++ LatinData.py
@@ -1,6 +1,6 @@
 import random
 #create dictionary for Latin adjectives
-latin_words = {
+latin_special_adj = {
     "Unus":["one"],
     "nUllus":["none","no"],
     "Ullus":["any"],
@@ -13,6 +13,136 @@ latin_words = {
 }
 
 #randomize dictionary for iteration
-list_of_lat_words = list(latin_words.items())
+list_of_lat_words = list(latin_special_adj.items())
 random.shuffle(list_of_lat_words)
-randomized_lat_dic = dict(list_of_lat_words)
+randomized_lat_adj_dic = dict(list_of_lat_words)
+
+#latin declensions
+first_declension = {
+    "Puella": {
+        "singular": {
+            "Nominative":"puella",
+            "Genitive":"puellae",
+            "Dative":"puellae",
+            "Accusative":"puellam",
+            "Ablative":"puellA",
+            "Vocative":"puella"
+        },
+        "plural" :{
+            "Nominative":"puellae",
+            "Genitive":"puellArum",
+            "Dative":"puellIs",
+            "Accusative":"puellAs",
+            "Ablative":"puellIs",
+            "Vocative":"puellae"
+        }
+    }
+}
+
+second_declension = {
+    "AmIcus": {
+        "singular": {
+            "Nominative":"amIcus",
+            "Genitive":"amIcI",
+            "Dative":"amIcO",
+            "Accusative":"amIcum",
+            "Ablative":"amIcO",
+            "Vocative":"amIce"
+        },
+        "plural" :{
+            "Nominative":"amIcI",
+            "Genitive":"amIcOrum",
+            "Dative":"amIcIs",
+            "Accusative":"amIcOs",
+            "Ablative":"amIcIs",
+            "Vocative":"amIcI"
+        }
+    }
+}
+
+second_declension_neuter = {
+    "Bellum": {
+        "singular": {
+            "Nominative":"bellum",
+            "Genitive":"bellI",
+            "Dative":"bellO",
+            "Accusative":"bellum",
+            "Ablative":"bellO",
+            "Vocative":"bellum"
+        },
+        "plural" :{
+            "Nominative":"bella",
+            "Genitive":"bellOrum",
+            "Dative":"bellIs",
+            "Accusative":"bella",
+            "Ablative":"bellIs",
+            "Vocative":"bella"
+        }
+    }
+}
+
+third_declension = {
+    "REx": {
+        "singular": {
+            "Nominative":"rEx",
+            "Genitive":"rEgis",
+            "Dative":"rEgI",
+            "Accusative":"rEgem",
+            "Ablative":"rEge",
+            "Vocative":"rEx"
+        },
+        "plural" :{
+            "Nominative":"rEgEs",
+            "Genitive":"rEgum",
+            "Dative":"rEgibus",
+            "Accusative":"rEgEs",
+            "Ablative":"rEgibus",
+            "Vocative":"rEgEs"
+        }
+    },
+    "VirtUs": {
+        "singular": {
+            "Nominative":"virtUs",
+            "Genitive":"virtUtis",
+            "Dative":"virtUtI",
+            "Accusative":"virtUtem",
+            "Ablative":"virtUte",
+            "Vocative":"virtUs"
+        },
+        "plural" :{
+            "Nominative":"virtUtEs",
+            "Genitive":"virtUtum",
+            "Dative":"virtUtibus",
+            "Accusative":"virtUtEs",
+            "Ablative":"virtUtibus",
+            "Vocative":"virtUtEs"
+        }
+    },
+        "Corpus": {
+        "singular": {
+            "Nominative":"corpus",
+            "Genitive":"corporis",
+            "Dative":"corpI",
+            "Accusative":"corpus",
+            "Ablative":"corpore",
+            "Vocative":"corpus"
+        },
+        "plural" :{
+            "Nominative":"corpora",
+            "Genitive":"corporur",
+            "Dative":"corporibus",
+            "Accusative":"corpora",
+            "Ablative":"corporibus",
+            "Vocative":"corpora"
+        
+        }
+    }
+}
+
+
+print(first_declension["Puella"]["singular"]["Genitive"])
+print(second_declension["AmIcus"]["singular"]["Genitive"])
+print(second_declension_neuter["Bellum"]["singular"]["Genitive"])
+print(third_declension["REx"]["singular"]["Genitive"])
+print(third_declension["VirtUs"]["singular"]["Genitive"])
+print(third_declension["Corpus"]["singular"]["Genitive"])
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from LatinData import randomized_lat_dic
+from LatinData import randomized_lat_adj_dic
 from quiz import quiz_func
 
 
-quiz = input("Do you want to start your quiz on special latin adjectives? Enter y or n: ")
-if quiz.lower() == "y":
-    words_missed = quiz_func(randomized_lat_dic)
+quiz = input("Latin Program: \n 1 Special Adjectives Translation \n 2 Declensions" \
+" \n 3 Verb Conjugations \n 4 Demonstratives \n 5 Authentic Latin Text Translations \n Enter Num Here->")
+if int(quiz) == 1:
+    words_missed = quiz_func(randomized_lat_adj_dic)
     while len(words_missed) != 0:
         retry_errors_answer = input("Would you like to repeat the words you missed? Enter y or n " )
         if retry_errors_answer.lower() == "y":
@@ -13,6 +14,8 @@ if quiz.lower() == "y":
             print("Bye!")
     else:
         print("0 words missed. Great job!")
+elif int(quiz) == 2:
+    print("In progress.")
 else:
-    print("Bye!")
+    print("Haven't started yet.")
 #07/04/26 Made small move over to linux
