542919307326b5ef834b77997a9bf73602fa333e
Joey <joey83891@gmail.com>
2026-07-26 08:25:51 -0400

    Cleaned up main using new logic function from quiz file

--- main.py
+++ main.py
@@ -15,24 +15,17 @@ if int(which_quiz) == 1:
     else:
         print("0 words missed. Great job!")
 elif int(which_quiz) == 2:
-    which_dec = input(" 1 for First Declension \n 2 for Second Declension \n 2.5 for Second Declension Neuter \n 3 for Third Declension \n Enter Num Here-> ")
+    which_dec = input(" 1 for First Declension " \
+    "\n 2 for Second Declension" \
+    " \n 2.5 for Second Declension Neuter \n 3 for Third Declension \n Enter Num Here-> ")
     if float(which_dec) == 1:
-        #  words_missed1d = q.decl_func(L.first_declension)
-        #  while len(words_missed1d) != 0:
-        #     retry_errors_answer1d = input("Would you like to repeat the words you missed? Enter y or n " )
-        #     if retry_errors_answer1d.lower() == "y":
-        #         words_missed1d = q.decl_func(words_missed1d)
-        #     else:
-        #         print("Bye!")
         q.main_choice_retry_logic(q.decl_func,L.first_declension)
     elif float(which_dec) == 2:
-        words_missed2d = q.decl_func(L.second_declension)
-        while len(words_missed2d) != 0:
-            retry_errors_answer2d = input("Would you like to repeat the words you missed? Enter y or n " )
-            if retry_errors_answer2d.lower() == "y":
-                words_missed2d = q.decl_func(words_missed2d)
-            else:
-                print("Bye!")
+        q.main_choice_retry_logic(q.decl_func, L.second_declension)
+    elif float(which_dec) == 2.5:
+        q.main_choice_retry_logic(q.decl_func,L.second_declension_neuter)
+    elif float(which_dec) == 3:
+        q.main_choice_retry_logic(q.decl_func,L.third_declension)
 else:
     print("Haven't started yet.")
 #07/04/26 Made small move over to linux
