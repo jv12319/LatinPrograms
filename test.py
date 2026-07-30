8d268a1f231b1e8529d20f1cb91c40841a394106
Joey <joey83891@gmail.com>
2026-07-30 07:15:52 -0400

    Improved conjugation function quiz, it works for now but will be redone asap. Also need to rework my current and future dictionaries.

--- test.py
+++ test.py
@@ -15,4 +15,32 @@ import LatinData as ll
 #     for attribute, answer in attributes.items():
 #         print(str(attribute)+": "+str(answer))
         
-print(ll.third_declension["VirtUs"])
+#print(ll.third_declension["VirtUs"])
+#print(f"{ll.first_conjugation["LaudAre"]["Present"]}")
+
+# name = "Joseph"
+# print(name[-3:])
+
+verbs = {"LaudAre" :{
+                "Principal Parts":["laudO", "laudAre", "laudAvI", "laudAtum"],
+                "Conjugation" :"1st",
+                "English Meaning" :["to praise", "to approve", "to extol"]
+}
+}
+
+conjugations = {"indicative active" :{
+    "1st" :{
+        "Present" :{
+            "1st pers sg":"O",
+            "2nd pers sg":"As",
+            "3rd pers sg":"at",
+            "1st pers pl":"Amus",
+            "2nd pers pl":"Atis",
+            "3rd pers pl":"ant",
+        }
+    }
+}}
+
+print(f"{verbs['LaudAre']['Principal Parts'][0][:-1]+conjugations['indicative active']['1st']['Present']['3rd pers sg']}")
+
+#def test_conj(verb_dict, conjugations_dict):
